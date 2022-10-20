2d270e5b45a27a81cd9883430fa1b6615661aced
ecy5045 <114458816+ecy5045@users.noreply.github.com>
2022-10-19 21:05:24 -0400

    Update PWMTest.py

--- PWMTest.py
+++ PWMTest.py
@@ -6,12 +6,12 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(servopin, GPIO.OUT)
 
-pi_pwm = GPIO.PWM(servopin, 333)
-pi_pwm.start(0)
+pi_pwm = GPIO.PWM(servopin, 333)		#assigns pin to new pwm instance with frequency of 333 Hz
+pi_pwm.start(0)					#starts pwm at duty cycle 0%
 
 while True:
-	pi_pwm.ChangeDutyCycle(5)
-	sleep(5)
+	pi_pwm.ChangeDutyCycle(5)		#changes duty cycle to inputed value (0-100%), this sets duty cycle to 5%
+	sleep(5)				#delay for 5 seconds
 	pi_pwm.ChangeDutyCycle(50)
 	sleep(5)
 	pi_pwm.ChangeDutyCycle(75)
